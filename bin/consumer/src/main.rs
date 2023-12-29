27f0acd6128ad005833054f2430b9181d5be45c2
0xMelkor <andreasimeoni84@gmail.com>
2023-12-29 11:41:32 +0100

    producer consumer works

--- bin/consumer/src/main.rs
+++ bin/consumer/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use rocksdb::{Options, DB};
+use std::{env, thread::sleep, time::Duration};
+
+fn main() {
+    loop {
+        let path = env::var("ROCKSDB_PATH").expect("ROCKSDB_PATH env");
+        let db = DB::open_for_read_only(&Options::default(), path, false).unwrap();
+
+        match db.get(b"my key") {
+            Ok(Some(value)) => println!("retrieved value {:?}", value),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("operational problem encountered: {}", e),
+        }
+
+        sleep(Duration::from_millis(100));
+    }
+}
--- bin/producer/src/main.rs
+++ bin/producer/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use std::{env, thread::sleep, time::Duration};
+
+use rocksdb::{DB, Options};
+
+fn main() {
+    let path = env::var("ROCKSDB_PATH").expect("ROCKSDB_PATH env");
+
+    let mut opts = Options::default();
+    opts.set_write_buffer_size(128 * 1024);
+    
+    // NB: db is automatically closed at end of lifetime
+    let db = DB::open(&opts, path).unwrap();
+
+    let mut num: u8  = 0;
+    loop {
+        println!("{num}");
+        let bytes = [num];
+        db.put(b"my key", bytes).expect("write");
+        num = (num + 1) % 255;
+        sleep(Duration::from_millis(10));
+    }
+}
